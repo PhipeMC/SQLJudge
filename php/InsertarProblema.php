2094371da6345628bc6dc6887de1a4048de94387
Daniel García Paniagua <danielgarcia488.dg@gmail.com>
2021-10-21 19:55:50 -0500

    Modificaciones a la base de datos, inserción de problemas y etiquetas para el archivo HTML

--- php/InsertarProblema.php
+++ php/InsertarProblema.php
@@ -7,12 +7,29 @@
 	$tema = $_POST['tema'];
 	$descripcion = $_POST['description'];
 	$consulta = $_POST['consulta'];
+	$dificultad = $_POST['dificultad'];
+
+	
+	if($dificultad=='Básico'){
+		$dificultad='Basico';
+	}
+
+	$resultado = mysqli_query($conexion, sprintf("SELECT idCATEGORIA from categoria where NombreCategoria='%s';",$tema));
+	$categoria = mysqli_fetch_array($resultado);
+
+	echo $nombre; echo "<br>";
+	echo $db; echo "<br>";
+	echo $tema; echo "<br>";
+	echo $descripcion; echo "<br>";
+	echo $consulta; echo "<br>";
+	echo $dificultad; echo "<br>";
+	echo $categoria[0]; echo "<br>";;
 
 	if($conexion){
 		echo "Conexion exitosa <br>";
 
-		$sentencia = "INSERT into problema (Titulo, Descripcion, DOCENTE_idUsuario, Solucion, CATEGORIA_idCATEGORIA) VALUES ('%s', '%s', 1, '%s', 1)";
-		$sql = sprintf($sentencia, $nombre, $descripcion, $consulta);
+		$sentencia = "INSERT into problema (Titulo, Descripcion, DOCENTE_idUsuario, Solucion, CATEGORIA_idCATEGORIA, dificultad) VALUES ('%s', '%s', 1, '%s', %d, '%s');";
+		$sql = sprintf($sentencia, $nombre, $descripcion, $consulta, $categoria[0] , $dificultad);
 
 		if(mysqli_query($conexion, $sql)){
 			echo "Elemento agregado";
@@ -21,7 +38,7 @@
 		}
 
 	}else{
-		die("Conexión Fallida: " . mysqli_connect_error());
+		echo "Conexión Fallida";
 	}
 
 
