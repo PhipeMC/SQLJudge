874b7e49f9ee324c1e5f49b97af5308686ccacbe
Frank55-T <fransc46@hotmail.com>
2021-10-27 18:46:54 -0500

    Diferentes BD
    Algoritmo en PHP para comparar códigos MySQL y otorgar un resultado

--- php/Conexion.php
+++ php/Conexion.php
@@ -13,6 +13,18 @@
 
 		return $conexion;
 	}
+	function conectarPorBD($BD){
 
+		$servername = "localhost";  //Servidor
+		$database = $BD;	//Base de datos
+		$username = "root";		//Usuario
+		$password = "Tulumbas500*";			//Contraseña
+
+		// Crear conexión
+
+		$conexion = mysqli_connect($servername, $username, $password, $database);
+
+		return $conexion;
+	}
 	
 ?>
