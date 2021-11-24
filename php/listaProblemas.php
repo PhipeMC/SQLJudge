e04b246716dfe14b3e5f9ceb73b698fa1860ae32
LeonardoIGZ <leoguz079@hotmail.com>
2021-11-24 02:02:59 -0600

    Cambios

--- php/listaProblemas.php
+++ php/listaProblemas.php
@@ -16,10 +16,6 @@ session_start();
     <link rel="stylesheet" href="../css/style-main.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
     <link rel="" href="https://cdn.datatables.net/fixedheader/3.1.6/css/fixedHeader.dataTables.min.css">
-<<<<<<< HEAD
-
-=======
->>>>>>> 088ce909bf5f5fafb1f6a4fdd084320f722badee
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 </head>
 
@@ -27,11 +23,7 @@ session_start();
     <div>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
-<<<<<<< HEAD
-                <a class="navbar-brand" href="index.php" id="logo"><i class="fas fa-terminal" style="color: #0247fe;"></i> SQL Code Judge</a>
-=======
-                <a class="navbar-brand" href="../index.html" id="logo"><i class="fas fa-terminal" style="color: #0247fe;"></i> SQL Code Judge</a>
->>>>>>> 088ce909bf5f5fafb1f6a4fdd084320f722badee
+                <a class="navbar-brand" href="../index.php" id="logo"><i class="fas fa-terminal" style="color: #0247fe;"></i> SQL Code Judge</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
