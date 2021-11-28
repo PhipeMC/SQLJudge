b8978f7d8a8f47858d50dffde74a72b70748d110
Frank55-T <fransc46@hotmail.com>
2021-11-28 00:41:30 -0600

    dsfds
    tegsfeadsdg

---
+++ js/problemaMarkDownImport.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    var text = document.getElementById("inputDescripcion").value,
+        target = document.getElementById("targetDiv"),
+        converter = new showdown.Converter(),
+        html = converter.makeHtml(text);
+    target.innerHTML = html;
+
+
+}
